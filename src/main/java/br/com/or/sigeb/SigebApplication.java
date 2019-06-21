f39cec5498feceef060afe2b97d7b74b08fe819a
Odenilton Lima <odeniltonlj@gmail.com>
2019-06-21 18:56:16 -0300

    Commit initial

---
+++ src/main/java/br/com/or/sigeb/SigebApplication.java
@@ -0,0 +1,13 @@
+package br.com.or.sigeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigebApplication.class, args);
+	}
+
+}
